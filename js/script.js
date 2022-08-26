248193e901b5ca97eb2e54a97e92845e838ae0cc
Nali1919 <my@email.com>
2022-08-26 17:17:08 +0200

    Creo struttura e genero numeri casuali

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Js Ok!')
+
+const numeroUtente = Math.floor(Math.random() * 6) +1;
+
+console.log(numeroUtente)
+
+const numeroPc = Math.floor(Math.random() * 6) +1;
+
+console.log(numeroPc)
+
