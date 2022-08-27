3368b89b33b7f090b1ac587729e45d354c6e8c8c
Nali1919 <my@email.com>
2022-08-27 11:58:32 +0200

    Aggiungo risultato su html

--- js/script.js
+++ js/script.js
@@ -4,16 +4,44 @@ const numeroUtente = Math.floor(Math.random() * 6) +1;
 
 console.log(numeroUtente)
 
+document.getElementById('utente').innerHTML = numeroUtente
+
 const numeroPc = Math.floor(Math.random() * 6) +1;
 
 console.log(numeroPc)
+
+document.getElementById('pc').innerHTML = numeroPc
+
+const risultato = document.getElementById('risultato')
+
+const solution = document.createElement('div')
+
+risultato.append(solution)
+
+let result 
 
 
 
 if (numeroUtente > numeroPc)
     
-    console.log('Vincitore utente')
+    result='Vincitore utente'
+    
+    console.log(result)
+
+    solution.innerHTML= (result)
 
 if (numeroPc > numeroUtente)
+    result='Vincitore Pc'
 
-    console.log('Vincitore Pc')
+    console.log(result)
+
+    solution.innerHTML= (result)
+
+if (numeroPc === numeroUtente)
+    result='X'
+
+    console.log(result)
+
+    solution.innerHTML= (result)
+
+    
